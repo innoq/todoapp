37278031d5cd8504cf1d13af30885fdf1c10274c
Torsten Mandry <torsten.mandry@innoq.com>
2022-01-19 09:43:54 +0100

    Typisierung korrigiert

--- src/main/java/com/innoq/praktikum/todoapp/AufgabenListe.java
+++ src/main/java/com/innoq/praktikum/todoapp/AufgabenListe.java
@@ -4,20 +4,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class AufgabenListe {
-    private List aufgaben;
+    private List<Aufgabe> aufgaben;
 
     public AufgabenListe() {
-        this.aufgaben = new ArrayList();
+        this.aufgaben = new ArrayList<>();
     }
 
     public void neueAufgabe(String bezeichnung) {
-        Object Aufgabe = new Aufgabe(bezeichnung, false);
-        aufgaben.add(bezeichnung);
+        Aufgabe aufgabe = new Aufgabe(bezeichnung, false);
+        aufgaben.add(aufgabe);
     }
 
     public void offeneAufgaben() {
         for (int i=0; i<aufgaben.size(); i++) {
-            Object aufgabe = aufgaben.get(i);
+            Aufgabe aufgabe = aufgaben.get(i);
             System.out.println(aufgabe);
         }
     }
